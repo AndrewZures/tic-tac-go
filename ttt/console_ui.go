87cd0a975ab8a1680a8d122ee6f0a0760c20169c
Andrew Zures <azures@8thlight.com>
2014-02-03 18:44:19 -0600

    can test output

--- ttt/console_ui.go
+++ ttt/console_ui.go
@@ -1,16 +1,22 @@
 package ttt
 
-import "fmt"
+import ("fmt"
+        "io"
+      )
 
 type ConsoleUI struct {
+  Writer io.Writer;
 }
 
-func (c *ConsoleUI) PrintBoard(boardArray []int) (bool) {
+func (c *ConsoleUI) PrintBoard(boardArray []int) {
 
-    for i := 0; i < len(boardArray); i++ {
-      fmt.Printf("value at %v is %v\n", i, boardArray[i])
-    }
+  for i := 0; i < len(boardArray); i++ {
+    fmt.Fprintf(c.Writer, "value at %v is %v\n", i, boardArray[i])
+  }
 
-    return true
+}
+
+func (c *ConsoleUI) Print (input string){
+  fmt.Fprintln(c.Writer, input)
 }
 
--- ttt_test/console_ui_test.go
+++ ttt_test/console_ui_test.go
@@ -4,18 +4,27 @@ import (
 	. "github.com/andrewzures/tictactoe/ttt"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+   "bytes"
 )
 
 var _ = Describe("Console UI", func() {
-    var console *ConsoleUI
+    var console ConsoleUI
+    var writer bytes.Buffer
     var board []int
 
     BeforeEach(func(){
+      console = ConsoleUI{&writer}
       board = make([]int, 9, 9)
     })
 
-    It("meets player interface requirements", func() {
-        Expect(console.PrintBoard(board)).To(Equal(true))
+    It("prints using variable io.Writer options ", func() {
+      console.Print("hello world")
+      Expect(writer.String()).To(ContainSubstring("hello world\n"))
+    })
+
+    It("prints a board", func() {
+      console.PrintBoard(board)
+      Expect(writer.String()).To(ContainSubstring("value at 1 is 0\n"))
     })
 
 })
